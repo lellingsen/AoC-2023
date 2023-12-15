dcd3bb611cebb612d4ad2ec17050a46ec77261a5
Lars Ellingsen <lars@datavant.com>
2023-12-15 07:41:30 -0800

    Day 15 part A solution

---
+++ 15a.py
@@ -0,0 +1,25 @@
+from aoc import get_input
+
+input = get_input(15)
+# input = 'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'
+
+def run_hash(str):
+    value = 0
+
+    for character in str:
+        ascii_code = ord(character)
+        value += ascii_code
+        value *= 17
+        value %= 256
+
+    return value
+
+steps = input.split(',')
+sum = 0
+
+for step in steps:
+    value = run_hash(step)
+    print(step, value)
+    sum += value
+
+print(sum)
